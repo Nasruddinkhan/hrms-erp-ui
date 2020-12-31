4be9a5cd08649e30fa2553969cc6bd10eb9c131e
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-01-01 01:15:46 +0530

    set up project

---
+++ src/main/java/com/mypractice/hrms/erp/HrmsErpUiApplication.java
@@ -0,0 +1,13 @@
+package com.mypractice.hrms.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsErpUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsErpUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypractice/hrms/erp/HrmsErpUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypractice.hrms.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsErpUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
